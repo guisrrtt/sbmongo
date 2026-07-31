af82083295b9cb543b51ba14032d6041f9a2f5a5
Guilherme Serretti <serrettiguilherme@gmail.com>
2026-07-31 17:21:05 -0300

    Inserção de usuário com POST

--- src/main/java/com/guiserretti/sbmongo/services/UserService.java
+++ src/main/java/com/guiserretti/sbmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guiserretti.sbmongo.domain.User;
+import com.guiserretti.sbmongo.dto.UserDTO;
 import com.guiserretti.sbmongo.repository.UserRepository;
 import com.guiserretti.sbmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }
