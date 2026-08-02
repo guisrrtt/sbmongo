7b7fe3a1f9ea35f206371ec6394bc57baf8ff24a
Guilherme Serretti <serrettiguilherme@gmail.com>
2026-08-01 23:59:53 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/guiserretti/sbmongo/config/Instantiation.java
+++ src/main/java/com/guiserretti/sbmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.guiserretti.sbmongo.domain.Post;
 import com.guiserretti.sbmongo.domain.User;
+import com.guiserretti.sbmongo.dto.AuthorDTO;
 import com.guiserretti.sbmongo.repository.PostRepository;
 import com.guiserretti.sbmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/07/2026"), "Férias!", "Vou passar duas semanas em Tamandaré, abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("25/07/2026"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/07/2026"), "Férias!", "Vou passar duas semanas em Tamandaré, abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("25/07/2026"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
