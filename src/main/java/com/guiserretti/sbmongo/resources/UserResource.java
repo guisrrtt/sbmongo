5c52ad4fca9a560086edf48bc1d7c52b2639cc20
Guilherme Serretti <serrettiguilherme@gmail.com>
2026-08-02 00:17:14 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/guiserretti/sbmongo/resources/UserResource.java
+++ src/main/java/com/guiserretti/sbmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.guiserretti.sbmongo.domain.Post;
 import com.guiserretti.sbmongo.domain.User;
 import com.guiserretti.sbmongo.dto.UserDTO;
 import com.guiserretti.sbmongo.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
