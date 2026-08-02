510613a877ce775cdf6582eb5a4f14d01938eb86
Guilherme Serretti <serrettiguilherme@gmail.com>
2026-08-02 00:22:47 -0300

    Obtendo post por id

---
+++ src/main/java/com/guiserretti/sbmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.guiserretti.sbmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guiserretti.sbmongo.domain.Post;
+import com.guiserretti.sbmongo.repository.PostRepository;
+import com.guiserretti.sbmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
